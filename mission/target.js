b4929f7bb2156d693ce8a4d77a72c7aa1515a8a1
Matthew Edgar <edgarmatt26@byui.edu>
2025-05-03 18:22:36 -0600

    light and dark set up

--- mission/target.js
+++ mission/target.js
@@ -1,16 +1,16 @@
 let selectElem = document.querySelector("select");
 let logo = document.querySelector("img");
+let body = document.body;
 
 selectElem.addEventListener("change", changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
-    if (current == "dark"){
-        // change body to dark
-        //change logo to the new logo
+    if (current === "dark") {
+        body.classList.add("dark-mode");
+        logo.src = "images/byui-logo_white.png"; 
     } else {
-        //remove dark class from body
-        //change logo back to og
+        body.classList.remove("dark-mode");
+        logo.src = "images/byui-logo_blue.jpg";
     }
 }
-//testing
