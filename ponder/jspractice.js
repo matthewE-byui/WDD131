c1cb4d4620f28356f6546be0d7fe7725c2f8d952
Matthew Edgar <edgarmatt26@byui.edu>
2025-05-03 18:04:43 -0600

    testing

---
+++ ponder/jspractice.js
@@ -0,0 +1,22 @@
+const PI  = 3.14
+
+let radius = 3;
+
+let area = radius * radius * PI;
+
+
+
+
+
+
+
+console.log(area);
+
+const one = 1;
+const two = 2;
+
+let result = one * two;
+
+console.log(result);
+
+result
---
+++ ponder/target.js
@@ -0,0 +1,7 @@
+const introParagraph = document.querySelector("p.para#intro");
+introParagraph.style.backgroundColor = "#f2c556";
+
+
+const emphasis = document.querySelector("em");
+emphasis.innerText = "USS Voyager StarShip";
+
