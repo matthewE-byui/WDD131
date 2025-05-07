e50d83eecc30fc88aded4599099428ce1628c1d0
Matthew Edgar <edgarmatt26@byui.edu>
2025-05-07 12:30:29 -0600

    CoolPics responsive gallery project

---
+++ ponder/cool pics/coolpics.js
@@ -0,0 +1,5 @@
+// You can expand this later for interactivity
+document.getElementById('menu').addEventListener('click', () => {
+    document.querySelector('nav').classList.toggle('open');
+  });
+  
